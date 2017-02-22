d03367c4f973ef5ec349087ba4ea9c2739a89376
ChaitanyaShetty <chaithanyavenk@gmail.com>
2017-02-22 09:53:48 -0200

    Initial commit

---
+++ PassData/TableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.m
+//  PassData
+//
+//  Created by chaitanya on 16/09/16.
+//  Copyright © 2016 chaitanya. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
